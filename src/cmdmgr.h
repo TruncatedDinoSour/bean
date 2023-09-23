228bcb68e1bdc788a67d903dba0344ed2df23be4
Ari Archer <ari.web.xyz@gmail.com>
2023-09-23 22:36:59 +0300

    make commands return an unsigned char type rather than bigger short
    Signed-off-by: Ari Archer <ari.web.xyz@gmail.com>

--- src/cmdmgr.h
+++ src/cmdmgr.h
@@ -3,12 +3,12 @@
 #include "types.h"
 
 #define CMD_NAME(name) cmd_##name
-#define CMD_DEF(name)  short CMD_NAME(name)(String *)
-#define CMD(name)      short CMD_NAME(name)(String argv[])
+#define CMD_DEF(name)  unsigned char CMD_NAME(name)(String *)
+#define CMD(name)      unsigned char CMD_NAME(name)(String argv[])
 
 typedef struct {
     String name, desc;
-    short (*cmd)(String *);
+    unsigned char (*cmd)(String *);
 } Command;
 
 const Command *find_command(String, const Command *);
